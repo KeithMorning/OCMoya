43e67655c0803de18908f7dca499f45dc276f5fd
keith <ustbfxx@163.com>
2017-03-25 11:16:54 +0800

    Initial Commit

---
+++ OCMoya/OCMoya.h
@@ -0,0 +1,19 @@
+//
+//  OCMoya.h
+//  OCMoya
+//
+//  Created by keith on 25/03/2017.
+//  Copyright © 2017 keithxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OCMoya.
+FOUNDATION_EXPORT double OCMoyaVersionNumber;
+
+//! Project version string for OCMoya.
+FOUNDATION_EXPORT const unsigned char OCMoyaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCMoya/PublicHeader.h>
+
+
